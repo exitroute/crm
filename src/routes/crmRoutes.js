14cb85b488ac9d4bdf7ff3611f5b067cee6c5337
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-10-08 01:22:12 +0200

    add routes

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,22 +1,26 @@
+import {
+  addNewContact,
+  getContacts,
+  getContactsWithID,
+  updateContact,
+  deleteContact,
+} from "../controllers/crmController";
+
 const routes = (app) => {
   app
     .route("/contact")
-    .get(
-      (req, res, next) => {
-        console.log(`Request from: ${req.originalUrl}`);
-        console.log(`Request type: ${req.method}`);
-        next();
-      },
-      (req, res, next) => {
-        res.send("GET request successful!");
-      }
-    )
-    .post((req, res) => res.send("POST request successful!"));
+    .get((req, res, next) => {
+      console.log(`Request from: ${req.originalUrl}`);
+      console.log(`Request type: ${req.method}`);
+      next();
+    }, getContacts)
+    .post(addNewContact);
 
   app
     .route("/contact/:contactID")
-    .put((req, res) => res.send("PUT request successful!"))
-    .delete((req, res) => res.send("DELETE request successful!"));
+    .get(getContactsWithID)
+    .put(updateContact)
+    .delete(deleteContact);
 };
 
 export default routes;
